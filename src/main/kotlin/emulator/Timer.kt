6cef329c4f157476765a18baca57938beb0f045d
Rylei Mindrum <A02352206@aggies.usu.edu>
2024-08-03 12:49:17 -0600

    Atomic for pausing Timer

--- src/main/kotlin/emulator/Timer.kt
+++ src/main/kotlin/emulator/Timer.kt
@@ -1,5 +1,12 @@
 package emulator
 
+import java.util.concurrent.atomic.AtomicBoolean
+
 class Timer {
     var value: Byte = 0
 }
+
+object pauseTimer {
+    val pauseTimer = AtomicBoolean(false)
+}
+// Atomic Boolean: A boolean value that may be updated atomically
