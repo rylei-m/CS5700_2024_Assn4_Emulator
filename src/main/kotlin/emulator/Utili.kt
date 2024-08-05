8989da8213a0dfbeeeea2fffd9dedb898ca6e54c
Rylei Mindrum <A02352206@aggies.usu.edu>
2024-08-05 03:35:48 -0600

    funcs  fpr emulator

--- src/main/kotlin/emulator/Utili.kt
+++ src/main/kotlin/emulator/Utili.kt
@@ -1,5 +1,8 @@
 package emulator
 
+import emulator.architecture.memory.base.RamNRom.ManageRom
+import emulator.architecture.memory.base.RamNRom.ManageRom.rom
+import emulator.architecture.memory.base.RamNRom.Rom
 import java.io.File
 import java.io.IOException
 
@@ -12,9 +15,53 @@ class Utili {
     fun getBinary(pathToBinaryFile: String): File {
         val file = File(pathToBinaryFile)
         if (!file.exists()) {
-            throw IOException("file does not exist")
+            throw IOException("File does not exist at path: $pathToBinaryFile")
         }
         return file
     }
+    fun byteArrayToInt(byteArray: ByteArray) : Int {
+        require(ByteArray.size == 2)
+        val result = (byteArray[1].toInt() and 0xFF) or ((byteArray[0].toInt() and 0xFF) shl 8)
+        return result
+    }
 
+    fun readNextInstructionBytes(): ByteArray {
+        return try {
+            val bi = byteArrayToInt(p.read())
+            val byte1 = rom?.read(bi) ?: 0
+            val byte2 = rom?.read(bi + 1) ?: 0
+            byteArrayOf(byte1, byte2)
+        } catch (e: Exception) {
+            byteArrayOf(0, 0)
+        }
+
+    }
+
+    fun getPathToBinary(): String {
+        val pathToBinary = readlnOrNull() ?: throw IOException("Path cannot be null")
+        return pathToBinary
+    }
+
+
+
+    fun binaryProgramFromFile(binaryFile: File): ByteArray {
+        return try {
+            binaryFile.readBytes()
+        } catch (e:IOException) {
+            throw IOException()
+        }
+    }
+
+    private fun romFromBinaryProgram(binaryProgram: ByteArray): Rom {
+        val memory = ByteArray(4096)
+        for (i in binaryProgram.indices) {
+            memory[1] = binaryProgram[i]
+        }
+        ManageRom.initializeRom(memory)
+        val rom = ManageRom.getRom()
+        if (rom ==null) {
+            throw IOException()
+        }
+        return rom
+    }
 }
