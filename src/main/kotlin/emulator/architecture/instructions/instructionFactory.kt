9244e3275ca37a2ca65d973c4ae95937bb30b546
Rylei Mindrum <A02352206@aggies.usu.edu>
2024-08-05 15:24:27 -0600

    Added instructionset to factory

--- src/main/kotlin/emulator/architecture/instructions/instructionFactory.kt
+++ src/main/kotlin/emulator/architecture/instructions/instructionFactory.kt
@@ -1,8 +1,25 @@
 package emulator.architecture.instructions
 
+import emulator.architecture.instructions.set.Add
+
 class instructionFactory {
     private val instructions = arrayOf(
         ::Add,
+        ::Sub,
+        ::Read,
+        ::Write,
+        ::Jump,
+        ::ReadKeyboard,
+        ::SwitchMemory,
+        ::SkipEqual,
+        ::SkipNotEqual,
+        ::SetA,
+        ::SetT,
+        ::ReadT,
+        ::ConvertToBase10,
+        ::ConvertByteToAscii,
+        ::Draw
+
     )
 
 
@@ -10,7 +27,7 @@ class instructionFactory {
         fun createInstruction(nibble0: Byte, nibble1: Byte, nibble2: Byte, nibble3: Byte, nibble31: Byte): Instruction {
             val instructionConstructor = instructions[nibble0.toInt()]
 
-            //val instruction = instructionConstructor(byteArrayOf(nibble1, nibble2, nibble3))
+            val instruction = instructionConstructor(byteArrayOf(nibble1, nibble2, nibble3))
             return instructionConstructor(byteArrayOf(nibble1, nibble2, nibble3))
         }
     }
